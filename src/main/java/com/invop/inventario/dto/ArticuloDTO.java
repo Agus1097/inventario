35da43dc7783e0beab65359ad8f6e492717c7d3c
Bruno <brunix.palombarini@gmail.com>
2025-06-01 20:01:04 -0300

    articulo dto

--- src/main/java/com/invop/inventario/dto/ArticuloDTO.java
+++ src/main/java/com/invop/inventario/dto/ArticuloDTO.java
@@ -1,4 +1,31 @@
 package com.invop.inventario.dto;
 
+import com.invop.inventario.entities.Proveedor;
+import com.invop.inventario.entities.TipoModelo;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
 public class ArticuloDTO {
+
+
+    private Long id;
+    private Long codArticulo;
+    private String nombre;
+    private String descripcion;
+    private float demandaArticulo;
+    private float costoAlmacenamiento;
+    private float costoPedido;
+    private float costoCompra;
+    private LocalDate fechaBajaArticulo;
+    private int puntoPedido;
+    private int stockSeguridad;
+    private int inventarioMaximo;
+    private int loteOptimo;
+    private int stockActual;
+
+    private ProveedorDTO proveedorPredeterminado;
+
+    //habria q hacer dto de esto?
+    private TipoModelo tipoModelo;
 }
