bc1afccdcd98f57a51571ac1b7047a95f950405b
Manuel Vazquez <manuuva5@gmail.com>
2025-06-17 23:52:02 -0300

    endpoints de productos /a-reponer y /faltantes

--- src/main/java/com/invop/inventario/repositories/OrdenCompraRepository.java
+++ src/main/java/com/invop/inventario/repositories/OrdenCompraRepository.java
@@ -3,10 +3,9 @@ package com.invop.inventario.repositories;
 import com.invop.inventario.entities.Articulo;
 import com.invop.inventario.entities.EstadoOrden;
 import com.invop.inventario.entities.OrdenCompra;
-import com.invop.inventario.entities.Proveedor;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
